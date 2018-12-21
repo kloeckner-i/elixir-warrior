41a0c34e086ffba9e86db67ab039633d1af2a555
Nick Reynolds <nick.reynolds@kloeckner.com>
2018-12-21 14:54:25 +0100

    Adds map parsing

--- lib/towers/one.ex
+++ lib/towers/one.ex
@@ -6,9 +6,47 @@ defmodule ElixirWarrior.Towers.One do
   def map do
     """
      --------
-    |@      >|
+    |@       |
+    |        |
+    |       >|
      --------
     """
+  end
+
+  def parse_square(?-), do: :horizontal_wall
+  def parse_square(?|), do: :vertical_wall
+  def parse_square(?@), do: :warrior
+  def parse_square(?>), do: :stairs
+
+  def parse_line(y, line) do
+    chars =
+      line
+      |> to_charlist()
+      |> Enum.with_index()
+
+    for {ch, x} <- chars,
+      ch != 32,
+      do: {{x, y}, parse_square(ch)},
+      into: %{}
+  end
+
+  def parse_map(map) do
+    rows =
+      map
+      |> String.trim_trailing()
+      |> String.split(~r/\R/)
+      |> Enum.with_index()
+
+    for {row, y} <- rows do
+      parse_line(y, row)
+    end
+
+    # %{
+    #   {1,0} => :wall,
+    #   {2,0} => :wall,
+    #   {1,1} => :warrior,
+    #   {3,7} => :stairs
+    # }
   end
 
   def description do
@@ -21,7 +59,9 @@ defmodule ElixirWarrior.Towers.One do
 
   def objects, do: []
 
-  def stairs_at, do: {7, 0}
+  def stairs_at, do: {7, 3}
+
+  def bounds, do: {{0,0}, {7,3}}
 
   def warrior do
     %Warrior{
