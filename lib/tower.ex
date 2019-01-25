5128ed0a3b913e40018bc03443cff023f43ba9c8
Nick Reynolds <nick.reynolds@kloeckner.com>
2019-01-25 12:29:05 +0100

    Adds doctests to tower, support arbitrary dimensions

--- lib/tower.ex
+++ lib/tower.ex
@@ -9,22 +9,45 @@ defmodule ElixirWarrior.Tower do
 
   @callback warrior() :: Warrior.t()
 
-  def parse_cell(?-), do: :horizontal_wall
-  def parse_cell(?|), do: :vertical_wall
-  def parse_cell(?@), do: :warrior
-  def parse_cell(?>), do: :stairs
+  @doc """
+  Parses a binary represenation of a floor plan into a map.
 
-  def display_cell(:horizontal_wall), do: "-"
-  def display_cell(:vertical_wall), do: "|"
-  def display_cell(:warrior), do: "@"
-  def display_cell(:stairs), do: ">"
-  def display_cell(:space), do: " "
+  ## Examples
 
-  def parse_line(y, line) do
-    chars =
-      line
-      |> to_charlist()
-      |> Enum.with_index()
+      iex> Tower.parse_floor_plan(
+      ...>   \"""
+      ...>   -----
+      ...>   |@ >|
+      ...>   -----
+      ...>   \"""
+      ...> )
+      %{
+        {0, 0} => :horizontal_wall,
+        {0, 1} => :vertical_wall,
+        {0, 2} => :horizontal_wall,
+        {1, 0} => :horizontal_wall,
+        {1, 1} => :warrior,
+        {1, 2} => :horizontal_wall,
+        {2, 0} => :horizontal_wall,
+        {2, 2} => :horizontal_wall,
+        {3, 0} => :horizontal_wall,
+        {3, 1} => :stairs,
+        {3, 2} => :horizontal_wall,
+        {4, 0} => :horizontal_wall,
+        {4, 1} => :vertical_wall,
+        {4, 2} => :horizontal_wall
+      }
+  """
+  def parse_floor_plan(text) do
+    text
+    |> String.trim_trailing()
+    |> String.split(~r/\R/)
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {line, y}, acc -> Map.merge(acc, parse_line(y, line)) end)
+  end
+
+  defp parse_line(y, line) do
+    chars = line |> to_charlist() |> Enum.with_index()
 
     for {ch, x} <- chars,
         ch != ?\s,
@@ -32,29 +55,59 @@ defmodule ElixirWarrior.Tower do
         into: %{}
   end
 
-  def parse_floor_plan(floor_plan) do
-    floor_plan
-    |> String.trim_trailing()
-    |> String.split(~r/\R/)
-    |> Enum.with_index()
-    |> Enum.reduce(%{}, fn {line, y}, acc -> Map.merge(acc, parse_line(y, line)) end)
-  end
+  defp parse_cell(?-), do: :horizontal_wall
+  defp parse_cell(?|), do: :vertical_wall
+  defp parse_cell(?@), do: :warrior
+  defp parse_cell(?>), do: :stairs
 
-  def display(parsed_floor_plan) do
-    for y <- 0..5, x <- 0..10 do
-      Map.get(parsed_floor_plan, {x, y}, :space)
-      |> display_cell()
-      |> new_line(x)
+
+  @doc """
+  Converts a map representation of a floor plan to a binary.
+
+  ## Examples
+
+      iex> Tower.display(
+      ...>   %{
+      ...>     {0, 0} => :horizontal_wall,
+      ...>     {0, 1} => :vertical_wall,
+      ...>     {0, 2} => :horizontal_wall,
+      ...>     {1, 0} => :horizontal_wall,
+      ...>     {1, 1} => :warrior,
+      ...>     {1, 2} => :horizontal_wall,
+      ...>     {2, 0} => :horizontal_wall,
+      ...>     {2, 2} => :horizontal_wall,
+      ...>     {3, 0} => :horizontal_wall,
+      ...>     {3, 1} => :stairs,
+      ...>     {3, 2} => :horizontal_wall,
+      ...>     {4, 0} => :horizontal_wall,
+      ...>     {4, 1} => :vertical_wall,
+      ...>     {4, 2} => :horizontal_wall
+      ...>   }
+      ...> )
+      "-----\\n|@ >|\\n-----\\n"
+  """
+  def display(plan) do
+    x_max = Enum.max(for {{x, _}, _} <- plan, do: x)
+    y_max = Enum.max(for {{_, y}, _} <- plan, do: y)
+
+    for y <- 0..y_max, x <- 0..x_max do
+      cell =
+        plan
+        |> Map.get({x, y}, :space)
+        |> display_cell()
+
+      if x == x_max do
+        cell <> "\n"
+      else
+        cell
+      end
     end
     |> Enum.join()
-    |> IO.puts()
   end
 
-  def new_line(char, 10) do
-    char <> "\n"
-  end
-
-  def new_line(char, _x) do
-    char
-  end
+  defp display_cell(:horizontal_wall), do: "-"
+  defp display_cell(:vertical_wall), do: "|"
+  defp display_cell(:warrior), do: "@"
+  defp display_cell(:stairs), do: ">"
+  defp display_cell(:space), do: " "
 end
